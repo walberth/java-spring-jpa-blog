ee2d0a67f4d7d903aebcf623d5d3af3080172fc6
Walberth <w.felipe.gutierrez@gmail.com>
2020-04-20 19:02:19 -0500

    se obtiene la data de la base de datos

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -5,13 +5,18 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String lead;
+    @Lob
+    @Column(length=1000000)
     private String body;
     private String author;
+    @Temporal(TemporalType.DATE)
     private Date date;
 
     public Post() {
